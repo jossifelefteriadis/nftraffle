7ee3af0cf83018813edafa1ee535697f2c26517c
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2022-03-05 10:28:00 +0100

    Complete site with sign up form working

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MailchimpContainer from "./MailchimpContainer";
 
 const ComingSoon = () => (
   <section className="comingsoon">
@@ -9,16 +10,16 @@ const ComingSoon = () => (
           We're creating what you've been waiting for.
           <br />
           This website is for the NFT community, by the NFT community - because
-          it's time to give back. Join free raffles and have a chance to win
+          it's time to give back. Join FREE raffles and have a chance to win
           tokens and NFT's on multiple networks.
         </p>
         <br />
         <p>
-          We aim on launching around mid-March. Make sure you sign up below to
-          get notified for when it's time.
+          We aim on launching around end of March. Make sure you sign up below
+          to get notified for when it's time.
         </p>
       </section>
-      <section>
+      {/* <section>
         <input
           class="signup_field"
           type="search"
@@ -28,11 +29,19 @@ const ComingSoon = () => (
         <button type="submit" class="signup_bth">
           SIGN UP
         </button>
-      </section>
+      </section> */}
+      <MailchimpContainer />
       <section>
         <p>
-          Also, follow us on twitter to get more updates on what's happening and
-          upcoming raffles.
+          Also, follow us on{" "}
+          <a
+            href="https://twitter.com/nftrafflewin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            twitter
+          </a>{" "}
+          to get more updates on what's happening and upcoming raffles.
         </p>
       </section>
     </section>
---
+++ src/components/MailchimpContainer.js
@@ -0,0 +1,61 @@
+import React from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+
+const CustomForm = ({ status, message, onValidated }) => {
+  let email;
+
+  const submit = () =>
+    email &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value,
+    });
+
+  return (
+    <section>
+      <br />
+      {status === "sending" && <section>sending...</section>}
+      {status === "error" && (
+        <section
+          style={{ color: "red" }}
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      {status === "success" && (
+        <section dangerouslySetInnerHTML={{ __html: message }} />
+      )}
+      <section>
+        <input
+          className="signup_field"
+          ref={(node) => (email = node)}
+          type="email"
+          placeholder="put email here"
+        />
+      </section>
+      <button class="signup_btn" label="subscribe" onClick={submit}>
+        SIGN UP
+      </button>
+    </section>
+  );
+};
+
+const MailchimpContainer = () => {
+  const url = `https://hotmail.us14.list-manage.com/subscribe/post?u=79596d12ef3d1416aa6defd18&id=dad8246b1a`;
+
+  return (
+    <section>
+      <MailchimpSubscribe
+        url={url}
+        render={({ subscribe, status, message }) => (
+          <CustomForm
+            status={status}
+            message={message}
+            onValidated={(formData) => subscribe(formData)}
+          />
+        )}
+      />
+    </section>
+  );
+};
+
+export default MailchimpContainer;
